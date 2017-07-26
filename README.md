44de9ecd719c40c8a1c4813051b2adecb14b9433
Marcus Warnsley <marcus@pc0159-jscott.users.directemployers.com>
2017-07-26 11:08:39 -0400

    Editing the port process

---
+++ README.md
@@ -0,0 +1,7 @@
+#Notification System
+
+A simple notification system for saving notifications to a users profile for shipment
+
+#To Run
+
+Clone this repository. Go into the folder and run npm install. You will also need to run your local Mongodb server from port 27018. Inside of the folder main directory, run npm start to start the server. Open another terminal in the same directory and run webpack to run webpack compiling. Your server should be started on port 5000 localhost:5000.
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 var path = require('path');
 var port = 5000;
 
+// Set the port to use
+app.set('port', (process.env.PORT || port));
+
 // Middleware to define folder for static files
 app.use(express.static('public'));
 
@@ -12,6 +15,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Application listening on port: ${port}`);
+app.listen(app.get('port'), () => {
+  console.log(`Application listening on port: `, app.get('port'));
 });
